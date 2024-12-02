14a4828a643c0bd918977bda35490b799318432e
arhamansari11 <arhamansari454647@gmail.com>
2024-12-02 09:21:46 +0500

    Advent of Code Day-02

---
+++ Day-02/solution1.py
@@ -0,0 +1,7 @@
+# Question 1 Code
+
+
+# # Read the input file
+with open("e:/Advent of Code/Day-02/input1.txt") as f:
+    lines = f.read().strip().split("\n")
+
---
+++ Day-02/solution2.py
@@ -0,0 +1,6 @@
+# Question 2 Code
+
+
+# # Read the input file
+with open("e:/Advent of Code/Day-02/input2.txt") as f:
+    lines = f.read().strip().split("\n")
