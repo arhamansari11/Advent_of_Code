28be8e8f1642a697ba58ae2b6c2d959e9df43462
arhamansari11 <arhamansari454647@gmail.com>
2024-12-10 08:03:53 +0500

    Advent of Code Day-09

--- Day-09/solution1.py
+++ Day-09/solution1.py
@@ -1,7 +1,7 @@
 from pathlib import Path
 
 # fn = "ex1.txt"
-fn = "e:/Advent of Code/Day-08/input.txt"
+fn = "e:/Advent of Code/Day-09/input.txt"
 #fn = Path(Path(__file__).parent, fn)
 dat = open(fn).read().strip().split("\n")
 
