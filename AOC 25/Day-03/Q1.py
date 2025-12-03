b3e0741aab31906fd84ba1e296969e7fd3174f61
arhamansari11 <arhamansari454647@gmail.com>
2025-12-03 10:17:29 +0500

    Advent of Code Day 3

--- AOC 25/Day-03/Q1.py
+++ AOC 25/Day-03/Q1.py
@@ -1,2 +1,31 @@
-"E:\Advent of Code\AOC 25\Day-03\Input.txt"
 
+
+# day3_lobby.py
+
+def max_joltage_from_bank(bank_line: str) -> int:
+    """
+    Given a string of digits, pick two digits (in order)
+    that form the largest possible number.
+    """
+    max_val = 0
+    n = len(bank_line)
+    for i in range(n - 1):
+        for j in range(i + 1, n):
+            num = int(bank_line[i] + bank_line[j])
+            if num > max_val:
+                max_val = num
+    return max_val
+
+
+def total_max_joltage(path="E:\Advent of Code\AOC 25\Day-03\Input.txt") -> int:
+    total = 0
+    with open(path, "r") as f:
+        for line in f:
+            line = line.strip()
+            if line:
+                total += max_joltage_from_bank(line)
+    return total
+
+
+if __name__ == "__main__":
+    print(total_max_joltage("E:\Advent of Code\AOC 25\Day-03\Input.txt"))
--- AOC 25/Day-03/Q2.py
+++ AOC 25/Day-03/Q2.py
@@ -1,3 +1,36 @@
-"E:\Advent of Code\AOC 25\Day-03\Input.txt"
 
 
+
+# day3_lobby_part2.py
+
+def max_number_from_digits(s: str, k: int) -> int:
+    """
+    Given a string s of digits, pick exactly k digits in order
+    to form the largest possible number.
+    """
+    stack = []
+    drop = len(s) - k  # number of digits we can remove
+
+    for digit in s:
+        while drop and stack and stack[-1] < digit:
+            stack.pop()
+            drop -= 1
+        stack.append(digit)
+
+    # In case we didn't drop enough, trim from the end
+    result_digits = stack[:k]
+    return int("".join(result_digits))
+
+
+def total_max_joltage_12(path="E:\Advent of Code\AOC 25\Day-03\Input.txt") -> int:
+    total = 0
+    with open(path, "r") as f:
+        for line in f:
+            line = line.strip()
+            if line:
+                total += max_number_from_digits(line, 12)
+    return total
+
+
+if __name__ == "__main__":
+    print(total_max_joltage_12("E:\Advent of Code\AOC 25\Day-03\Input.txt"))
